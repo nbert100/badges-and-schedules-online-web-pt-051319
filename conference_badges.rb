d69ab5779a1ef2cb3c0224da0e3c810ebf946bd4
Nia Bert <nbert14@gmail.com>
2019-05-18 14:42:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |name| 
+    new_array.push("Hello, my name is #{name}.")
+  end
+  return new_array
+end
+
+def assign_rooms(attendees)
+  room_list = []
+  room = index(name) + 1
+  attendees.each_with_index do |name, index|
+    room_list.push("Hello, #{name}! You'll be assigned to room #{room}!")
+  end
+  return room_list
+end
+    
