a72249b85778e8e3ab81366baa133d93ef87aa4d
Nia Bert <nbert14@gmail.com>
2019-05-18 15:17:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,7 +20,7 @@ def assign_rooms(attendees)
   return room_list
 end
     
-def printer(array)
-  puts batch_badge_creator
-  puts assign_rooms
+def printer(attendees)
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(att)
 end
